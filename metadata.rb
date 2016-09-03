541ae01a2e6c04ef141e8def9cd92efe282d8ef0
Gordon B. Isnor <gordonbisnor@gmail.com>
2016-09-02 21:46:04 -0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name              "nginx-tlq"
-maintainer        "Ben Dixon"
-maintainer_email  "ben@talkingquickly.co.uk"
+name              "nginx-gbi"
+maintainer        "Gordon Isnor"
+maintainer_email  "info@isnorcreative.com"
 description       "Installs the nginx web server from the nginx ppa"
-version           "0.0.4"
+version           "0.0.1"
 
-recipe "nginx-tlq", "nginx server"
+recipe "nginx-gbi", "nginx server"
 
 supports "ubuntu"
